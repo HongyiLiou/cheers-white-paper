b63ec4be6e70ffc34a5d7e0abfbbd8ab0cc9c1a7
BK <kelvinchen@cw.com.tw>
2022-10-14 15:18:12 +0800

    新增長條圖

--- assets/js/front-end/main-bk.js
+++ assets/js/front-end/main-bk.js
@@ -1,38 +1,38 @@
-const chartTransformX = 60;
-const chartTransformY = 40;
+const lineChartTransformX = 60;
+const lineChartTransformY = 40;
 let windowWidth = window.innerWidth;
-let viewBoxSize = {};
+let lineViewBoxSize = {};
 function setViewBox() {
-  viewBoxSize = {
+  lineViewBoxSize = {
     width: 470,
     height: 360
   };
   if (windowWidth <= 1399) {
-    viewBoxSize = {
+    lineViewBoxSize = {
       width: 440,
       height: 320
     }
   }
   if (windowWidth <= 1199) {
-    viewBoxSize = {
+    lineViewBoxSize = {
       width: 700,
       height: 430
     }
   }
   if (windowWidth <= 991) {
-    viewBoxSize = {
+    lineViewBoxSize = {
       width: 500,
       height: 430
     }
   }
   if (windowWidth <= 767) {
-    viewBoxSize = {
+    lineViewBoxSize = {
       width: 400,
       height: 300
     }
   }
   if (windowWidth <= 575) {
-    viewBoxSize = {
+    lineViewBoxSize = {
       width: windowWidth - 40,
       height: (windowWidth - 40) * 0.84
     }
@@ -42,7 +42,7 @@ setViewBox();
 
 
 function initTwVacanciesLineChart(targetWidth, targetHeight) {
-  var twVacancies = d3.select("#tw-vacancies_line-chart");
+  var twVacancies = d3.select("#tw_vacancies-line_chart");
   twVacancies.html("");
   var parent = twVacancies.select(function() {
     return this.parentNode
@@ -78,11 +78,11 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
   twVacancies.attr({
     width: parentWidth,
     height: 0.78 * parentWidth,
-    viewBox: `0 0 ${viewBoxSize.width} ${viewBoxSize.height}`,
+    viewBox: `0 0 ${lineViewBoxSize.width} ${lineViewBoxSize.height}`,
   });
 
   var scaleX = d3.scale.linear().range([0, width]).domain([0, 4]);
-  console.log(scaleX(1))
+  // console.log(scaleX(1))
 
   var scaleY = d3.scale.linear().range([height, 0]).domain([0, 600000]);
 
@@ -130,7 +130,7 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
     stroke: "#AE441F",
     "stroke-width": "2.5px",
     fill: "none",
-    transform: `translate(${chartTransformX}, ${chartTransformY})`, //折線圖也要套用 translate
+    transform: `translate(${lineChartTransformX}, ${lineChartTransformY})`, //折線圖也要套用 translate
   }).style({
     "stroke-dasharray": 1000,
     "stroke-dashoffset": 1000,
@@ -144,13 +144,13 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
     .attr({
       fill: "none",
       stroke: "rgba(0,0,0,.1)",
-      transform: `translate(${chartTransformX},${height + chartTransformY})`,
+      transform: `translate(${lineChartTransformX},${height + lineChartTransformY})`,
     });
 
   twVacancies.append("g").call(axisYGrid).attr({
     fill: "none",
     stroke: "rgba(0,0,0,.1)",
-    transform: `translate(${chartTransformX},${chartTransformY})`,
+    transform: `translate(${lineChartTransformX},${lineChartTransformY})`,
   });
 
   // Axis
@@ -159,7 +159,7 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
     .call(axisX)
     .attr({
       fill: "none",
-      transform: `translate(${chartTransformX},${height + chartTransformY})`,
+      transform: `translate(${lineChartTransformX},${height + lineChartTransformY})`,
     })
     .selectAll("text")
     .attr({
@@ -175,7 +175,7 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
     .call(axisY)
     .attr({
       fill: "none",
-      transform: `translate(${chartTransformX}, ${chartTransformY})`,
+      transform: `translate(${lineChartTransformX}, ${lineChartTransformY})`,
     })
     .selectAll("text")
     .attr({
@@ -189,7 +189,7 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
   twVacancies.append("text")
     .attr({
       "class": "x label",
-      "transform": `translate(${chartTransformX}, ${chartTransformY})`,
+      "transform": `translate(${lineChartTransformX}, ${lineChartTransformY})`,
       x: -45,
       y: -20
     })
@@ -197,7 +197,7 @@ function initTwVacanciesLineChart(targetWidth, targetHeight) {
 }
 
 function initWorkPeopleLineChart() {
-  var workPeople = d3.select("#work-people_line-chart");
+  var workPeople = d3.select("#work_people-line_chart");
   workPeople.html("");
   var parent = workPeople.select(function() {
     return this.parentNode
@@ -225,7 +225,7 @@ function initWorkPeopleLineChart() {
   workPeople.attr({
     width: parentWidth,
     height: 0.76 * parentWidth,
-    viewBox: `0 0 ${viewBoxSize.width} ${viewBoxSize.height}`,
+    viewBox: `0 0 ${lineViewBoxSize.width} ${lineViewBoxSize.height}`,
   });
 
   var scaleX = d3.scale.linear().range([0, width]).domain([0, 2]);
@@ -274,7 +274,7 @@ function initWorkPeopleLineChart() {
     stroke: "#AE441F",
     "stroke-width": "2.5px",
     fill: "none",
-    transform: `translate(${chartTransformX}, ${chartTransformY})`, //折線圖也要套用 translate
+    transform: `translate(${lineChartTransformX}, ${lineChartTransformY})`, //折線圖也要套用 translate
   }).style({
     "stroke-dasharray": 1000,
     "stroke-dashoffset": 1000,
@@ -288,13 +288,13 @@ function initWorkPeopleLineChart() {
     .attr({
       fill: "none",
       stroke: "rgba(0,0,0,.1)",
-      transform: `translate(${chartTransformX},${height + chartTransformY})`,
+      transform: `translate(${lineChartTransformX},${height + lineChartTransformY})`,
     });
 
   workPeople.append("g").call(axisYGrid).attr({
     fill: "none",
     stroke: "rgba(0,0,0,.1)",
-    transform: `translate(${chartTransformX}, ${chartTransformY})`,
+    transform: `translate(${lineChartTransformX}, ${lineChartTransformY})`,
   });
 
   // Axis
@@ -303,7 +303,7 @@ function initWorkPeopleLineChart() {
     .call(axisX)
     .attr({
       fill: "none",
-      transform: `translate(${chartTransformX},${height + chartTransformY})`,
+      transform: `translate(${lineChartTransformX},${height + lineChartTransformY})`,
     })
     .selectAll("text")
     .attr({
@@ -319,7 +319,7 @@ function initWorkPeopleLineChart() {
     .call(axisY)
     .attr({
       fill: "none",
-      transform: `translate(${chartTransformX}, ${chartTransformY})`,
+      transform: `translate(${lineChartTransformX}, ${lineChartTransformY})`,
     })
     .selectAll("text")
     .attr({
@@ -333,11 +333,290 @@ function initWorkPeopleLineChart() {
     workPeople.append("text")
       .attr({
         "class": "x label",
-        "transform": `translate(${chartTransformX}, ${chartTransformY})`,
+        "transform": `translate(${lineChartTransformX}, ${lineChartTransformY})`,
         x: -45,
         y: -20
       })
       .text("人數");
+}
+
+const barChartTransformX = 60;
+const barChartTransformY = 20;
+let barViewBoxSize = {};
+
+function setBarViewBox() {
+  barViewBoxSize = {
+    width: 460,
+    height: 340
+  };
+  if (windowWidth <= 1399) {
+    barViewBoxSize = {
+      width: 420,
+      height: 320
+    }
+  }
+  if (windowWidth <= 1199) {
+    barViewBoxSize = {
+      width: 750,
+      height: 480
+    }
+  }
+  if (windowWidth <= 991) {
+    barViewBoxSize = {
+      width: 500,
+      height: 430
+    }
+  }
+  if (windowWidth <= 767) {
+    barViewBoxSize = {
+      width: 400,
+      height: 300
+    }
+  }
+  if (windowWidth <= 575) {
+    barViewBoxSize = {
+      width: windowWidth - 40,
+      height: (windowWidth - 40) * 0.84
+    }
+  }
+}
+setBarViewBox();
+
+function initSkillWhenBarChart() {
+  const skillWhen = d3.select("#skill_when-bar_chart");
+
+  skillWhen.html("");
+  const parent = skillWhen.select(function() {
+    return this.parentNode
+  });
+  const parentWidth = parseInt(parent.style("width"));
+  const width = 0.7414448669201521 * parentWidth;
+  const height = 0.532319391634981 * parentWidth;
+
+  const data = [
+    {
+      x: "現在",
+      y: 43
+    },
+    {
+      x: "2年內",
+      y: 22
+    },
+    {
+      x: "3-5年內",
+      y: 22
+    },
+    {
+      x: "6-10年內",
+      y: 5
+    },
+    {
+      x: "10年以上",
+      y: 6
+    }
+  ]
+
+  skillWhen.attr({
+    width: parentWidth,
+    height: 0.7 * parentWidth,
+    viewBox: `0 0 ${barViewBoxSize.width} ${barViewBoxSize.height}`,
+  })
+
+  // Scales
+  const scaleX = d3.scale.ordinal()
+    .domain(data.map(function(data) { return data.x }))
+    .rangeRoundBands([0, width]);
+
+  const scaleY = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.y; }) * 1.1])
+    .range([height, 0]);
+
+  // axis
+  const axisX = d3.svg
+    .axis()
+    .scale(scaleX)
+    .orient("bottom")
+    .tickFormat(function (d) {
+      return `${d}`;
+    })
+    .tickPadding(10)
+
+  skillWhen.append("g")
+    .call(axisX)
+    .attr({
+      "fill": "none",
+      "class": "x axis",
+      "transform": `translate(${barChartTransformX}, ${height + barChartTransformY})`
+    })
+    .selectAll("text")
+    .attr({
+      fill: "#000",
+      stroke: "none",
+    })
+    .style({
+      "font-size": "14px",
+    });
+
+  const axisY = d3.svg
+    .axis()
+    .scale(scaleY)
+    .orient("left")
+    .tickValues([0, 10, 20, 30, 40])
+
+  skillWhen.append("g")
+    .call(axisY)
+    .attr({
+      "fill": "none",
+      "class": "y axis",
+      "transform": `translate(${barChartTransformX}, ${barChartTransformY})`
+    })
+    .selectAll("text")
+    .attr({
+      fill: "#000",
+      stroke: "none",
+    })
+    .style({
+      "font-size": "12px",
+    });
+
+  //bar
+  var bar = skillWhen.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr({
+        "fill": "#AE441F",
+        "class": "bar",
+        "y": height,
+        "height": 0,
+        transform: `translate(${barChartTransformX + scaleX.rangeBand() / 4}, ${barChartTransformY})`
+      })
+      .attr("x", function(d) { return scaleX(d.x); })
+      .attr("width", scaleX.rangeBand() / 2)
+
+  bar.transition()
+    .duration(1500)
+    .ease("elastic")
+    .attr("y", function(d) { return scaleY(d.y); })
+    .attr("height", function(d) { return height - scaleY(d.y); });
+}
+
+function initSkillIn3YearsBarChart() {
+  const skillWhen = d3.select("#skill_in_3years-bar_chart");
+
+  skillWhen.html("");
+  const parent = skillWhen.select(function() {
+    return this.parentNode
+  });
+  const parentWidth = parseInt(parent.style("width"));
+  const width = 0.7414448669201521 * parentWidth;
+  const height = 0.532319391634981 * parentWidth;
+
+  const data = [
+    {
+      x: "現在",
+      y: 43
+    },
+    {
+      x: "2年內",
+      y: 22
+    },
+    {
+      x: "3-5年內",
+      y: 22
+    },
+    {
+      x: "6-10年內",
+      y: 5
+    },
+    {
+      x: "10年以上",
+      y: 6
+    }
+  ]
+
+  skillWhen.attr({
+    width: parentWidth,
+    height: 0.7 * parentWidth,
+    viewBox: `0 0 ${barViewBoxSize.width} ${barViewBoxSize.height}`,
+  })
+
+  // Scales
+  const scaleX = d3.scale.ordinal()
+    .domain(data.map(function(data) { return data.x }))
+    .rangeRoundBands([0, width]);
+
+  const scaleY = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.y; }) * 1.1])
+    .range([height, 0]);
+
+  // axis
+  const axisX = d3.svg
+    .axis()
+    .scale(scaleX)
+    .orient("bottom")
+    .tickFormat(function (d) {
+      return `${d}`;
+    })
+    .tickPadding(10)
+
+  skillWhen.append("g")
+    .call(axisX)
+    .attr({
+      "fill": "none",
+      "class": "x axis",
+      "transform": `translate(${barChartTransformX}, ${height + barChartTransformY})`
+    })
+    .selectAll("text")
+    .attr({
+      fill: "#000",
+      stroke: "none",
+    })
+    .style({
+      "font-size": "14px",
+    });
+
+  const axisY = d3.svg
+    .axis()
+    .scale(scaleY)
+    .orient("left")
+    .tickValues([0, 10, 20, 30, 40])
+
+  skillWhen.append("g")
+    .call(axisY)
+    .attr({
+      "fill": "none",
+      "class": "y axis",
+      "transform": `translate(${barChartTransformX}, ${barChartTransformY})`
+    })
+    .selectAll("text")
+    .attr({
+      fill: "#000",
+      stroke: "none",
+    })
+    .style({
+      "font-size": "12px",
+    });
+
+  //bar
+  var bar = skillWhen.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr({
+      "fill": "#AE441F",
+      "class": "bar",
+      "y": height,
+      "height": 0,
+      transform: `translate(${barChartTransformX + scaleX.rangeBand() / 4}, ${barChartTransformY})`
+    })
+    .attr("x", function(d) { return scaleX(d.x); })
+    .attr("width", scaleX.rangeBand() / 2)
+
+  bar.transition()
+    .duration(3000)
+    .ease("elastic")
+    .attr("y", function(d) { return scaleY(d.y); })
+    .attr("height", function(d) { return height - scaleY(d.y); });
 }
 
 function d3Resize() {
@@ -346,11 +625,13 @@ function d3Resize() {
     setViewBox();
     initTwVacanciesLineChart();
     initWorkPeopleLineChart();
+    initSkillWhenBarChart();
   });
 }
 
 window.onload = function () {
   initTwVacanciesLineChart();
   initWorkPeopleLineChart();
+  initSkillWhenBarChart();
   d3Resize();
 };
